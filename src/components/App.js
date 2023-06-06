5cb1199eb6c464524c4d99243c5c4b9248e921ec
Paul Allen <Lancea818@gmail.com>
2023-06-06 16:09:09 -0400

    CHANGES

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const App = () => {
+  return (
+    <>
+    <div>
+      <p>Hi! I'm Paul Web Dev</p>
+    </div>
+    </>
+  )
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+
+  plugins: [new HTMLWebpackPlugin({
+                template: './src/index.html'
+  }
+  )
+],
+  target:"web",
+  devServer: {
+    port: "3000",
+
+    static : ["./public"],
+
+    open: true,
+    hot: true,
+    liveReload: true,
+  },
+
+  resolve: {
+    extensions: [".js",".jsx", ".json"],
+  },
+  module: {
+  rules: [
+    {   test: /\.js$/, 
+        exclude: "/node_modules", 
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env', '@babel/preset-react']
+            }
+        }
+    },
+    { test: /\.css$/, use: 'css-loader' },
+    
+  ],
+  },
+};
