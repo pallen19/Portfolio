e21e5a49ec10b38c9acbee5154b7265b83377525
Paul Allen <Lancea818@gmail.com>
2023-09-01 12:01:30 -0400

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,20 @@ import "../style.css";
 import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import Projects from "../pages/Projects";
 import Resume from "../pages/Resume";
+import { initializeApp } from "firebase/app";
 
 const App = () => {
+  const firebaseConfig = {
+    apiKey: "AIzaSyB2FdfeGUk8pDwllTvsvewkFaxesLfxzJs",
+    authDomain: "lakcnease.firebaseapp.com",
+    projectId: "lakcnease",
+    storageBucket: "lakcnease.appspot.com",
+    messagingSenderId: "1039935281139",
+    appId: "1:1039935281139:web:c54ad77f23f71caa5c6486"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
   return (
     <>
       <BrowserRouter>
